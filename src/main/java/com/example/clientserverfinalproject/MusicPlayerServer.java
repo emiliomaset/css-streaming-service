159216ea73ef4cf2ad09dc426cc5cea38d121c50
emiliomaset <145236437+emiliomaset@users.noreply.github.com>
2024-03-26 17:52:29 -0400

    Adding songs and searching songs work well!

--- src/main/java/com/example/clientserverfinalproject/MusicPlayerServer.java
+++ src/main/java/com/example/clientserverfinalproject/MusicPlayerServer.java
@@ -5,22 +5,30 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class MusicPlayerServer {
-    private static ServerSocket serverSocket;
-    private static final int PORT = 1234;
+    private static ServerSocket serverSocket1;
+    private static ServerSocket serverSocket2;
+    private static ServerSocket serverSocket3;
+    private static final int PORT1 = 1234;
+    private static final int PORT2 = 4321;
+    private static final int PORT3 = 1324;
 
     public static void main(String[] args) throws IOException {
 
         try {
-            serverSocket = new ServerSocket(PORT);
+            serverSocket1 = new ServerSocket(PORT1);
+            serverSocket2 = new ServerSocket(PORT2);
+            serverSocket3 = new ServerSocket(PORT3);
         } catch (IOException ioEx) {
             System.out.println("\nUnable to set up port!");
             System.exit(1);
         }
 
-        while (!serverSocket.isClosed()) {
-            Socket client = serverSocket.accept();
+        while (!serverSocket1.isClosed() & !serverSocket2.isClosed()) {
+            Socket client1 = serverSocket1.accept();
+            Socket client2 = serverSocket2.accept();
+            Socket client3 = serverSocket3.accept();
             System.out.println("\nNew client accepted.\n");
-            ClientHandler handler = new ClientHandler(client);
+            ClientHandler handler = new ClientHandler(client1, client2, client3);
             handler.start();
         }
 
